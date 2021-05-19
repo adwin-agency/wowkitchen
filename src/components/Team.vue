a640c3da8c59575df69c6fd9cda57bd08c4e9326
Ogurtsov Artem <ogurcov.artem2012@list.ru>
2021-05-19 12:12:40 +0300

    Classes were reassigned and added App-icon component

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -4,68 +4,73 @@
             <div class="container container_side-fixed">
                 <div class="team__list">
                     <div class="team__item">
-                        <img class="team__item-img" src="@/assets/img/team/AP.png" alt="avatar" >
-                        <p class="team__item-name">Александр Петров</p>
-                        <p class="team__item-post">директор компании</p>
-                        <p class="team__item-text">Многолетний опыт в сфере кухонь и постоянное совершенствование всех стадий создания кухонных гарнитуров позволило нам отладить работу всей компании, словно работу швейцарских часов. Все наши специалисты проходят тщательный отбор и четко следуют инструкциям. Важно, что все работники понимают нашу концепцию и философию – создавать кухни для наших клиентов – как для себя.
-                            <button class="team__item-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 20 20">
-                                    <path d="M2507,1833a10,10,0,1,0,10.01,10A10.006,10.006,0,0,0,2507,1833Zm0,18a8,8,0,1,1,8.01-8A8.013,8.013,0,0,1,2507,1851Zm1-9h3v2h-3v3h-2v-3h-3.01v-2H2506v-3h2v3Z" transform="translate(-2497 -1833)"/>
-                                </svg>
+                        <img class="team__img" src="@/assets/img/team/AP.jpg" alt="avatar" >
+                        <p class="team__name">Александр Петров</p>
+                        <p class="team__post">директор компании</p>
+                        <p class="team__text">Многолетний опыт в сфере кухонь и постоянное совершенствование всех стадий создания кухонных гарнитуров позволило нам отладить работу всей компании, словно работу швейцарских часов. Все наши специалисты проходят тщательный отбор и четко следуют инструкциям. Важно, что все работники понимают нашу концепцию и философию – создавать кухни для наших клиентов – как для себя.
+                            <button class="team__btn">
+                                <App-icon
+                                    name="plus"
+                                    class="team__btn-icon"
+                                />
                             </button>
                         </p>
                     </div>
 
                     <div class="team__item">
-                        <img class="team__item-img" src="@/assets/img/team/OC.png" alt="avatar" >
-                        <p class="team__item-name">Ольга Спицина</p>
-                        <p class="team__item-post">руководитель отдела продаж</p>
-                        <p class="team__item-text">Мы стараемся реагировать на каждую заявку оперативно – чтобы между звонком клиента в наш офис и выездом к нему на дом дизайнера проходило минимальное количество В случае дополнительных вопросов всегда подробно на них отвечаем. Мы ценим время наших клиентов, поэтому до нас всегда легко дозвониться – никаких «ожидайте, ваш номер в очередь 49-й» и прочего. Все наши специалисты проходят обязательный курс по грамотной речи. Закажите обратный звонок и убедитесь в этом сами! ;)
-                            <button class="team__item-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 20 20">
-                                    <path d="M2507,1833a10,10,0,1,0,10.01,10A10.006,10.006,0,0,0,2507,1833Zm0,18a8,8,0,1,1,8.01-8A8.013,8.013,0,0,1,2507,1851Zm1-9h3v2h-3v3h-2v-3h-3.01v-2H2506v-3h2v3Z" transform="translate(-2497 -1833)"/>
-                                </svg>
+                        <img class="team__img" src="@/assets/img/team/OC.jpg" alt="avatar" >
+                        <p class="team__name">Ольга Спицина</p>
+                        <p class="team__post">руководитель отдела продаж</p>
+                        <p class="team__text">Мы стараемся реагировать на каждую заявку оперативно – чтобы между звонком клиента в наш офис и выездом к нему на дом дизайнера проходило минимальное количество В случае дополнительных вопросов всегда подробно на них отвечаем. Мы ценим время наших клиентов, поэтому до нас всегда легко дозвониться – никаких «ожидайте, ваш номер в очередь 49-й» и прочего. Все наши специалисты проходят обязательный курс по грамотной речи. Закажите обратный звонок и убедитесь в этом сами! ;)
+                            <button class="team__btn">
+                                <App-icon
+                                    name="plus"
+                                    class="team__btn-icon"
+                                />
                             </button>
                         </p>
                     </div>
 
                     <div class="team__item">
-                        <img class="team__item-img" src="@/assets/img/team/CL.png" alt="avatar" >
-                        <p class="team__item-name">Степан Лялин</p>
-                        <p class="team__item-post">координатор отдела сборки</p>
-                        <p class="team__item-text">На наших плечах огромная ответственность – именно от грамотной сборки зависит итоговый результат. Ведь если на производстве все идеально, но сборщик
+                        <img class="team__img" src="@/assets/img/team/CL.jpg" alt="avatar" >
+                        <p class="team__name">Степан Лялин</p>
+                        <p class="team__post">координатор отдела сборки</p>
+                        <p class="team__text">На наших плечах огромная ответственность – именно от грамотной сборки зависит итоговый результат. Ведь если на производстве все идеально, но сборщик
                         подкачает – пиши-пропало. Слава Богу, в нашей компании подобралась отличная команда, где каждый знает своё дело на пятерку с плюсом. Собираем все быстро, четко и качественно. Бранных слов не знаем, зато помним про то, что, уходя, необходимо уносить с собой строительный мусор. 
-                            <button class="team__item-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 20 20">
-                                    <path d="M2507,1833a10,10,0,1,0,10.01,10A10.006,10.006,0,0,0,2507,1833Zm0,18a8,8,0,1,1,8.01-8A8.013,8.013,0,0,1,2507,1851Zm1-9h3v2h-3v3h-2v-3h-3.01v-2H2506v-3h2v3Z" transform="translate(-2497 -1833)"/>
-                                </svg>
+                            <button class="team__btn">
+                                <App-icon
+                                    name="plus"
+                                    class="team__btn-icon"
+                                />
                             </button>
                         </p>
 
                     </div>
 
                     <div class="team__item">
-                        <img class="team__item-img" src="@/assets/img/team/ML.png" alt="avatar" >
-                        <p class="team__item-name">Максим Липовецкий</p>
-                        <p class="team__item-post">старший дизайнер-замерщик</p>
-                        <p class="team__item-text">Моя задача – услышать пожелания клиента, дать несколько дельных советов по функционалу и дизайну будущего гарнитура, произвести замеры, а также нарисовать предварительный проект Если клиенту всё нравится, прекрасно! Есть нюансы, не вопрос – дорабатываем проект, доводим его до идеала. Затем запускаем производство и уже через месяц удобный и современный гарнитур радует хозяев! С собой всегда беру чемодан с образцами, чтобы наши клиенты могли выбирать не по картинке, а вживую.
-                            <button class="team__item-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 20 20">
-                                    <path d="M2507,1833a10,10,0,1,0,10.01,10A10.006,10.006,0,0,0,2507,1833Zm0,18a8,8,0,1,1,8.01-8A8.013,8.013,0,0,1,2507,1851Zm1-9h3v2h-3v3h-2v-3h-3.01v-2H2506v-3h2v3Z" transform="translate(-2497 -1833)"/>
-                                </svg>
+                        <img class="team__img" src="@/assets/img/team/ML.jpg" alt="avatar" >
+                        <p class="team__name">Максим Липовецкий</p>
+                        <p class="team__post">старший дизайнер-замерщик</p>
+                        <p class="team__text">Моя задача – услышать пожелания клиента, дать несколько дельных советов по функционалу и дизайну будущего гарнитура, произвести замеры, а также нарисовать предварительный проект Если клиенту всё нравится, прекрасно! Есть нюансы, не вопрос – дорабатываем проект, доводим его до идеала. Затем запускаем производство и уже через месяц удобный и современный гарнитур радует хозяев! С собой всегда беру чемодан с образцами, чтобы наши клиенты могли выбирать не по картинке, а вживую.
+                            <button class="team__btn">
+                                <App-icon
+                                    name="plus"
+                                    class="team__btn-icon"
+                                />
                             </button>
                         </p>
 
                     </div>
                 </div>
-                <div class="team__join">
-                    <div class="team__join-container">
-                        <svg class="team__join-icon" viewBox="-48 0 496 496"  xmlns="http://www.w3.org/2000/svg"><path d="m104 256h192c4.425781 0 8-3.585938 8-8v-144c0-57.34375-46.65625-104-104-104s-104 46.65625-104 104v144c0 4.414062 3.574219 8 8 8zm8-152c0-48.519531 39.480469-88 88-88s88 39.480469 88 88v136h-176zm0 0"/><path d="m376 144h-48c-4.425781 0-8 3.585938-8 8v120h-240v-120c0-4.414062-3.574219-8-8-8h-48c-13.230469 0-24 10.769531-24 24s10.769531 24 24 24h8v96c0 17.648438 14.351562 32 32 32h100.6875l-98.34375 98.34375c-2.289062 2.289062-2.976562 5.726562-1.734375 8.71875 1.230469 2.984375 4.160156 4.9375 7.390625 4.9375h16v17.472656c-9.289062 3.3125-16 12.113282-16 22.527344 0 13.230469 10.769531 24 24 24s24-10.769531 24-24c0-10.414062-6.710938-19.214844-16-22.527344v-17.472656h8c2.128906 0 4.160156-.839844 5.65625-2.34375l58.34375-58.34375v52.6875c0 4.414062 3.574219 8 8 8h8v17.472656c-9.289062 3.3125-16 12.113282-16 22.527344 0 13.230469 10.769531 24 24 24s24-10.769531 24-24c0-10.414062-6.710938-19.214844-16-22.527344v-17.472656h8c4.425781 0 8-3.585938 8-8v-52.6875l58.34375 58.34375c1.496094 1.503906 3.527344 2.34375 5.65625 2.34375h8v17.472656c-9.289062 3.3125-16 12.113282-16 22.527344 0 13.230469 10.769531 24 24 24s24-10.769531 24-24c0-10.414062-6.710938-19.214844-16-22.527344v-17.472656h16c3.230469 0 6.160156-1.953125 7.390625-4.9375 1.242187-2.992188.554687-6.429688-1.734375-8.71875l-98.34375-98.34375h100.6875c17.648438 0 32-14.351562 32-32v-96h8c13.230469 0 24-10.769531 24-24s-10.769531-24-24-24zm-280 336c-4.414062 0-8-3.59375-8-8s3.585938-8 8-8 8 3.59375 8 8-3.585938 8-8 8zm104 0c-4.414062 0-8-3.59375-8-8s3.585938-8 8-8 8 3.59375 8 8-3.585938 8-8 8zm104 0c-4.414062 0-8-3.59375-8-8s3.585938-8 8-8 8 3.59375 8 8-3.585938 8-8 8zm-288-312c0-4.40625 3.585938-8 8-8h40v16h-40c-4.414062 0-8-3.59375-8-8zm92.6875 248h-17.375l84.6875-84.6875v17.375zm99.3125 0h-16v-96h16zm83.3125 0-67.3125-67.3125v-17.375l84.6875 84.6875zm60.6875-128c0 8.824219-7.175781 16-16 16h-272c-8.824219 0-16-7.175781-16-16v-96h16v88c0 4.414062 3.574219 8 8 8h256c4.425781 0 8-3.585938 8-8v-88h16zm24-112h-40v-16h40c4.414062 0 8 3.59375 8 8s-3.585938 8-8 8zm0 0"/><path d="m192 192h16v16h-16zm0 0"/><path d="m200 32c-30.871094 0-56 25.121094-56 56v16h16v-16c0-22.054688 17.945312-40 40-40s40 17.945312 40 40-17.945312 40-40 40c-4.425781 0-8 3.585938-8 8v40h16v-32.566406c27.105469-3.898438 48-27.265625 48-55.433594 0-30.878906-25.128906-56-56-56zm0 0"/></svg>
-                        <div class="team__join-box">
-                            <p class="team__join-title">Становитесь частью команды</p> 
-                            <p class="team__join-text">Мы всегда рады новичкам в наших рядах. Посмотрите список вакансий, возможно, мы ищем именно вас?</p>
-                            <p class="team__join-text">Отпраывляйте свои резюме на почту<a class="team__join-link team__join-text_accent"> sample@sample.com </a>с пометкой «Хочу в команду»</p>  
-                        </div>
+                <div class="team-join">
+                    <App-icon
+                        name="vacancy"
+                        class="team-join__icon"
+                    />
+                    <div class="team-join__box">
+                        <p class="team-join__title">Становитесь частью команды</p> 
+                        <p class="team-join__text">Мы всегда рады новичкам в наших рядах. Посмотрите список вакансий, возможно, мы ищем именно вас?</p>
+                        <p class="team-join__text">Отпраывляйте свои резюме на почту<a class="team-join__link"> sample@sample.com </a>с пометкой «Хочу в команду»</p>  
                     </div>
                 </div>
             </div>
@@ -76,12 +81,14 @@
 </template>
 
 <script>
+import AppIcon from './base/AppIcon.vue'
 import Interesting from "./Interesting.vue"
 
 export default {
   name: 'Team',
   components: {
-    Interesting
+    Interesting,
+    AppIcon
   }
 }
 </script>
@@ -119,79 +126,84 @@ export default {
             margin: 45px 10px;
             max-width: 400px;
         }
-        &-img{
-            width: 100%;
-            margin-bottom: 20px;
-            border-bottom-right-radius: 30px;
-            box-shadow: 0 16px 17px -1px rgba(0,0,0,0.2);
-        }
-        &-name {
-            font-size: 16px;
-            font-weight: bold;
-            line-height: 1.5;
-        }
-        &-post {
-            font-size: 14px;
-            color: $color-lightviolet;
-            line-height: 1.714;
-            margin-bottom: 20px;
-        }
-        &-text {
+        
+    }
+    &__img {
+        width: 100%;
+        margin-bottom: 20px;
+        border-bottom-right-radius: 30px;
+        box-shadow: 0 16px 17px -1px rgba(0,0,0,0.2);
+    }
+    &__name {
+        font-size: 16px;
+        font-weight: bold;
+        line-height: 1.5;
+    }
+    &__post {
+        font-size: 14px;
+        color: $color-lightviolet;
+        line-height: 1.714;
+        margin-bottom: 20px;
+    }
+    &__text {
 
-        }
-        &-btn {
-            display: inline-block;
+    }
+    &__btn {
+        display: inline-block;
+        fill: $color-lightviolet;
+        &-icon {
             width: 20px;
-            fill: $color-lightviolet;
+            height: 20px;
         }
     }
-    &__join {
+    &-join {
         background-color: $color-lightgray;
         box-shadow: 0px 16px 38px 0px rgba(0, 0, 0, 0.08);
         border-bottom-right-radius: 80px;
         margin: 0 -$container-padding;
-
-        &-container {
-            max-width: 251px;
-            margin: 0 auto;
-            padding: 42px 10px;
-            @include media (md) {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                max-width: 555px;
-            }
-            @include media (lg) {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                max-width: 730px;
-            }
+        padding: 42px 35px;
+        @include media (md) {  
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 42px 55px;
         }
-        &-box {
+        @include media (lg) {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 42px 85px;
+        }
+        &__box {
 
         }
-        &-icon {
+        &__icon {
             width: 70px;
+            height: 85px;
             fill: $color-lightviolet;
             margin-bottom: 15px;
             @include media (md) {
                 width: 90px;
-                height: 100%;
+                height: 100px;
+                margin-right: 25px;
+                margin-bottom: 0;
+            }
+            @include media (lg) {
                 margin-right: 55px;
             }
         }
-        &-title {
+        &__title {
             color: $color-green;
             font-size: 16px;
             font-weight: bold;
             margin-bottom: 20px;
 
         }
-        &-link {
+        &__link {
             cursor: pointer;
         }
-        &-text {
+        &__text {
             font-size: 13px;
             line-height: 1.692;
             &_accent {
