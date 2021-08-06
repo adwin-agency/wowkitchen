9678f505ec983ae29fbff60b841ca5a882fd1aba
adwin-agency <adwin.developer@gmail.com>
2021-08-06 18:47:42 +0300

    delivery / team update

--- src/components/Delivery.vue
+++ src/components/Delivery.vue
@@ -1,226 +1,48 @@
 <template>
   <div class="delivery">
-    <div class="container delivery__container">
-      <div class="delivery__discription-container">
-        <h2 class="delivery__discription-title">Доставка</h2>
-        <h3 class="delivery__discription-subtitle">В пределах СПб, ЛО, Москвы</h3>
-        <p class="delivery__discription-text">
+    <div class="delivery__main">
+      <div class="container">
+        <h1 class="delivery__description-title">Доставка</h1>
+        <p class="delivery__description-subtitle">В пределах СПб, ЛО, Москвы</p>
+        <p class="delivery__description-text">
           Доставка осуществляется с Пн. по Сб. 10:00 до 21:00. Стоимость зависит от вашего местонахождения.
         </p>
-        <p class="delivery__discription-text">
+        <p class="delivery__description-text">
           Подробнее узнать о стоимости можно у оператора по телефону:
           <a
-            :href="`tel:${cityPhone}`"
-            class="delivery__discription_accent"
-          >{{cityPhone}}</a>
+            :href="`tel:${phone}`"
+            class="delivery__description_accent"
+          >{{phone}}</a>
         </p>
-        <!-- <h3 class="delivery__discription-subtitle">В ЛО</h3>
-        <p class="delivery__discription-text">
-          Доставка за КАД, по Ленинградской области и в другие регионы рассчитывается индивидуально при оформлении заказа или по телефону:<span class="delivery__discription_accent"> +7 (999) 999 - 99 - 99</span>
-          <span class="delivery__discription_accent"></span>
-        </p> -->
-        <h3 class="delivery__discription-subtitle">Подъём на этаж</h3>
-        <p class="delivery__discription-text">
+        <p class="delivery__description-subtitle">Подъём на этаж</p>
+        <p class="delivery__description-text">
           Подъем в квартиру без лифта - 200 рублей этаж. Подъем в квартиру на лифте - бесплатно.
-          <span class="delivery__discription_accent"></span>
+          <span class="delivery__description_accent"></span>
         </p>
       </div>
-      <form
-        class="delivery__form"
-        @submit="handleSubmit"
+    </div>
+    <div class="delivery__side">
+      <div
+        v-if="!$_media.sm"
+        class="delivery__side-box"
       >
-        <input
-          type="hidden"
-          name="type"
-          value="delivery"
+        <p class="delivery__side-message">Оперативная доставка <br>в удобное вам время</p>
+        <img
+          src="@/assets/img/cart.png"
+          alt
+          class="delivery__side-img"
         >
-        <input
-          type="hidden"
-          name="page"
-          :value="page"
-        >
-        <div class="delivery__form-content">
-          <h4 class="delivery__form-title">Расчитать стоимость доставки</h4>
-          <label
-            class="delivery__form-lable"
-            for="select-city"
-          >Укажите адрес и наш оператор свяжется с вами для обсуждения стоимости.</label>
-          <div class="delivery__form-container">
-            <fieldset class="delivery__form-box">
-              <AppSelect
-                v-if="cities"
-                :options="cities"
-                color="white"
-                name="city"
-                class="delivery__form-select-city"
-                @change="handleCityChange"
-              />
-              <AppTextField
-                label="Улица"
-                color="white"
-                type="text"
-                name="street"
-                required
-                class="delivery__form-field"
-                @change="handleAddressChange('street', $event)"
-              />
-              <AppTextField
-                label="Дом"
-                color="white"
-                type="text"
-                name="building"
-                required
-                class="delivery__form-field delivery__form-field_sm"
-                @input="handleNumberInput"
-                @change="handleAddressChange('building', $event)"
-              />
-              <AppTextField
-                label="Корпус"
-                color="white"
-                type="text"
-                name="corp"
-                required
-                class="delivery__form-field delivery__form-field_sm"
-                @input="handleNumberInput"
-                @change="handleAddressChange('corp', $event)"
-              />
-              <AppTextField
-                label="Контактный телефон"
-                color="white"
-                type="tel"
-                name="phone"
-                required
-                class="delivery__form-field"
-              />
-              <AppButton
-                title="Рассчитать"
-                type="submit"
-                class="delivery__form-button-sb"
-              />
-              <p
-                v-if="error"
-                class="delivery__form-error"
-              >Ошибка отправки. Попробуйте еще раз</p>
-            </fieldset>
-            <div
-              class="delivery__form-map"
-              id="map"
-            ></div>
-          </div>
-          <p class="delivery__form-policy">
-            Нажимая кнопку «Рассчитать», вы соглашаетесь c
-            <a
-              class="delivery__form-policy-link"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            > Политикой конфиденциальности</a>
-          </p>
-        </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import AppButton from './base/AppButton.vue'
-import AppSelect from './base/AppSelect.vue'
-import AppTextField from './base/AppTextField.vue'
-import useForms from '../composition/forms'
-
 export default {
   name: 'Delivery',
-  components: {
-    AppSelect,
-    AppTextField,
-    AppButton
-  },
-  setup() {
-    const { sending, error, page, handleSubmit } = useForms()
-    return {
-      sending,
-      error,
-      page,
-      handleSubmit
-    }
-  },
-  data() {
-    return {
-      map: null,
-      city: Object.values(this.$store.state.cities).length ? Object.values(this.$store.state.cities)[0] : null,
-      street: '',
-      building: '',
-      corp: ''
-    }
-  },
   computed: {
-    cities() {
-      return Object.values(this.$store.state.cities).length ? Object.values(this.$store.state.cities).map((i) => ({ title: i.name, value: i.code })) : null
-    },
-    cityPhone() {
-      return this.city?.phone
-    },
-    location() {
-      const address = []
-
-      this.street && address.push(this.street)
-      this.street && this.building && address.push(this.building)
-      this.street && this.building && this.corp && address.push('к' + this.corp)
-
-      return this.city?.name + (address.length ? ', ' + address.join(', ') : '')
-    }
-  },
-  watch: {
-    cities() {
-      this.city = Object.values(this.$store.state.cities)[0]
-
-      if (!window.myMap) {
-        this.initMap()
-      }
-    }
-  },
-  mounted() {
-    if (this.city) {
-      this.initMap()
-    }
-  },
-  unmounted() {
-    window.myMap = null
-  },
-  methods: {
-    handleCityChange(code) {
-      this.city = this.$store.state.cities[code]
-      this.setMap()
-    },
-
-    handleNumberInput(event) {
-      event.target.value = event.target.value.replace(/\D/, '')
-    },
-
-    handleAddressChange(name, event) {
-      this[name] = event.target.value
-      this.setMap()
-    },
-
-    initMap() {
-      const coords = [this.city.coords.lat, this.city.coords.long]
-
-      window.ymaps.ready(() => {
-        window.myMap = new window.ymaps.Map('map', {
-          center: coords,
-          controls: [],
-          zoom: 9
-        })
-      })
-    },
-
-    async setMap() {
-      console.log(this.location)
-      const res = await window.ymaps.geocode(this.location, { results: 1 })
-      const geoObject = res.geoObjects.get(0)
-      const bounds = geoObject.properties.get('boundedBy')
-
-      window.myMap.setBounds(bounds)
+    phone() {
+      return this.$store.state.cities[this.$store.state.activeCity]?.phone
     }
   }
 }
@@ -228,170 +50,147 @@ export default {
 
 <style lang="scss">
 .delivery {
-  &__container {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 30px;
-    @include media(md) {
-      align-items: unset;
-      padding-top: 50px;
-      flex-direction: row;
-      padding-bottom: 40px;
-    }
+  &__main {
+    padding: 30px 0;
   }
-  &__discription {
-    &-container {
-      @include media(lg) {
-        flex: 50%;
-        padding-right: 80px;
-      }
-    }
-    &-title {
-      font-size: 26px;
-      font-weight: bold;
-      @include media(lg) {
-        font-size: 50px;
-      }
-    }
+
+  &__description {
     &-subtitle {
+      font-weight: bold;
       font-size: 18px;
       padding-top: 30px;
-
-      @include media(lg) {
-        padding-top: 40px;
-      }
     }
 
     &-text {
       padding-top: 5px;
       font-size: 14px;
       line-height: 26px;
-
-      @include media(md) {
-        padding-top: 20px;
-      }
     }
+
     &_accent {
       font-weight: bold;
       padding-left: 4px;
       white-space: nowrap;
     }
   }
-  &__form {
-    border-radius: 4px;
-    background-color: rgb(243, 244, 249);
-    box-shadow: 0px 10px 24px 0px rgba(0, 0, 0, 0.1);
-    margin-top: 24px;
-    padding: 0 18px 20px;
-    width: 100vw;
 
-    @include media(sm) {
-      padding: 0 30px 20px;
-    }
-    @include media(md) {
-      margin-left: 20px;
-    }
-    @include media(lg) {
-      width: 700px;
-      padding: 0 0px 20px 35px;
-    }
-    &-content {
-      max-width: $container-max-width - $container-padding * 2;
-      margin: 0 auto;
+  &__side {
+    display: flex;
+    align-items: flex-end;
+    padding-top: 56%;
+    background-color: #ccc;
+    background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('~@/assets/img/delivery-bg.jpg');
+    background-size: cover;
+    background-position: center;
 
-      @include media(lg) {
-        max-width: none;
-      }
-    }
-    &-title {
-      font-size: 18px;
-      font-weight: bold;
-      line-height: 1.333;
-      padding-top: 65px;
-      padding-bottom: 5px;
-    }
-    &-lable {
-      font-size: 13px;
-      line-height: 1.538;
-      @include media(xl) {
-        font-size: 18px;
-      }
-    }
-    &-container {
-      display: flex;
-      justify-content: center;
-      margin-top: 35px;
-      margin-bottom: 20px;
-    }
     &-box {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      padding: 0;
-      margin: 0;
-      width: 100%;
+      margin-left: -30px;
+    }
 
-      @include media(lg) {
-        max-width: 280px;
-        width: auto;
-        flex: 1;
-        margin-right: 35px;
-      }
-    }
-    &-select-city {
-      width: 100%;
-    }
-    &-city-option {
-      font-size: 13px;
-      color: rgb(4, 185, 145);
+    &-message {
+      display: inline-block;
+      margin-left: -15px;
+      padding: 18px 55px 40px;
       font-weight: bold;
-      line-height: 3.077;
-    }
-    &-field {
-      padding-top: 23px;
-      width: 100%;
-
-      &_sm {
-        width: calc(50% - 10px);
-      }
-    }
-    &-map {
-      display: none;
-      background-color: #e3e3e3;
-      margin-left: auto;
-
-      @include media(lg) {
-        display: block;
-        width: 325px;
-      }
-    }
-    &-policy {
-      font-size: 10px;
-      color: rgb(172, 168, 195);
-      line-height: 1.6;
-      text-align: center;
-      &-link {
-        text-decoration: underline;
-      }
-    }
-    &-button-sb {
-      margin-top: 15px;
-      width: 100%;
-    }
-
-    &-error {
-      margin: 15px auto 0;
-      text-align: center;
       font-size: 14px;
-      color: #ff0000;
+      line-height: (23/18);
+      text-align: center;
+      background-image: url('~@/assets/img/message-l.svg');
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: center;
     }
   }
 
-  fieldset {
-    border: none;
-    padding: 0;
+  @include media(md) {
+    display: flex;
+
+    &__main {
+      flex: 1;
+      padding: 50px 0;
+
+      .container {
+        margin-right: 0;
+        max-width: calc(50% + 330px);
+        padding-right: 120px;
+      }
+    }
+
+    &__description {
+      &-text {
+        padding-top: 20px;
+      }
+    }
+
+    &__side {
+      width: 300px;
+      padding: 150px 0 50px;
+
+      &-box {
+        margin-left: -100px;
+      }
+
+      &-message {
+        margin-left: 110px;
+      }
+
+      &-img {
+        margin-top: -120px;
+        margin-bottom: -50px;
+        width: 290px;
+      }
+    }
+  }
+
+  @include media(lg) {
+    &__main {
+      .container {
+        max-width: calc(50% + 410px);
+        padding-right: 200px;
+      }
+    }
+
+    &__description {
+      &-title {
+        font-size: 50px;
+      }
+
+      &-subtitle {
+        padding-top: 40px;
+      }
+    }
+
+    &__side {
+      width: 460px;
+
+      &-box {
+        margin-left: -130px;
+      }
+
+      &-message {
+        margin-left: 160px;
+        padding: 30px 75px 60px;
+        font-size: 18px;
+      }
+
+      &-img {
+        margin-top: -160px;
+        width: 353px;
+      }
+    }
+  }
+
+  @include media(xl) {
+    &__main {
+      .container {
+        max-width: calc(50% + 530px);
+      }
+    }
+
+    &__side {
+      width: 540px;
+      padding: 188px 0 50px;
+    }
   }
 }
 </style>
