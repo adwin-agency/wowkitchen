c033a43ee4cd67574547fd1d3cb44a1e5bd0d989
adwin-agency <adwin.developer@gmail.com>
2021-08-12 18:48:33 +0300

    history mode change

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView'
 import AboutView from '../views/AboutView'
 import ArticleView from '../views/ArticleView'
@@ -178,7 +178,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   linkActiveClass: 'is-active',
   linkExactActiveClass: '',
