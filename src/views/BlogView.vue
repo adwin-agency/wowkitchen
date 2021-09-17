e8f666b72244b0033980e985fa9028380476a720
adwin-agency <adwin.developer@gmail.com>
2021-09-17 17:47:00 +0300

    blog show more

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -83,7 +83,7 @@ export default {
 
     async showMore() {
       this.currentPage++
-      const response = await api.loadCards(this.$route, this.currentPage)
+      const response = await api.loadCards({...this.$route, query: {page: this.currentPage}})
       this.cards = [...this.cards, ...response.goods]
     }
   }
